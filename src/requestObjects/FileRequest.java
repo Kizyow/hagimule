d5420a3202b128c9a8e4169d368b082bd72a5e24
Pierre S <axiolerre@gmail.com>
2024-12-13 09:24:54 +0100

    Ajout tableau byte dans FileRequest pour contenu morceau

--- src/requestObjects/FileRequest.java
+++ src/requestObjects/FileRequest.java
@@ -2,7 +2,7 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-public class FileRequest implements Serializable {
+public class FileRequest {
     String fileName;
     List<FileRequestChunk> fileChunks = new ArrayList<>();
 
--- src/requestObjects/FileRequestChunk.java
+++ src/requestObjects/FileRequestChunk.java
@@ -4,11 +4,13 @@ public class FileRequestChunk implements Serializable {
     String fileName;
     int startingByte;
     int endingByte;
+    byte[] chunk;
 
     public FileRequestChunk(String fileName, int startingByte, int endingByte) {
         this.fileName = fileName;
         this.startingByte = startingByte;
         this.endingByte = endingByte;
+        chunk = new byte[endingByte-startingByte];
     }
 
     public String getFileName() {
@@ -23,5 +25,11 @@ public class FileRequestChunk implements Serializable {
         return endingByte;
     }
 
-    
+    public byte[] getChunk() {
+        return chunk;
+    }
+
+    public void setChunk(byte[] chunk) {
+        this.chunk = chunk;
+    }
 }
